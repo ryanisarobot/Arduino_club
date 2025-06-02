0e80bfbc5153ef9bea2666882a092510650727f8
ryan <144874436+ryanisarobot@users.noreply.github.com>
2025-06-02 00:56:56 -0700

    Update Reflex tester.ino

--- Reflex tester/Reflex tester.ino
+++ Reflex tester/Reflex tester.ino
@@ -1,47 +1,61 @@
-//problems are that lights is always green
 #define RED_PIN 7
 #define GREEN_PIN 2
 #define BLUE_PIN 4
 #define BUTTON_PIN 8
 
 unsigned long set_time;
+bool testStarted = false; // Track if the reaction test has officially started
 
 void setup() {
+  Serial.begin(9600);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
+  randomSeed(analogRead(0));
+  set_time = millis() + random(6000, 11000);
+
   digitalWrite(RED_PIN, HIGH);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, LOW);
 
-  randomSeed(analogRead(0)); 
-  unsigned long set_time = random(6000, 11000);
-  Serial.println(set_time);
-
-  Serial.begin(9600);
+  Serial.println("Reaction time test starts soon...");
 }
 
 void loop() {
-  int buttonState = digitalRead(BUTTON_PIN);
   unsigned long time = millis();
+  int buttonState = digitalRead(BUTTON_PIN);
 
-  if(time >= 10 && time < 70){
-    Serial.println("Welcome to reflex test. Once the light turns green, press the button as fast as you can. Get ready the lights will change any second.");
+  if (!testStarted && time >= 500) {
+    Serial.println("Get ready! Press the button as fast as you can when the light turns GREEN.");
+    testStarted = true;
   }
-  if(time >= set_time){
+
+  // Trigger the green light after the random set_time
+  if (testStarted && time >= set_time) {
     digitalWrite(GREEN_PIN, HIGH);
     digitalWrite(RED_PIN, LOW);
 
-    if(buttonState == LOW){
+    if (buttonState == LOW) { 
       unsigned long reaction_time = time - set_time;
-      Serial.println("You pressed the button in ");
-      Serial.println(reaction_time);
+      Serial.print("Your reaction time: ");
+      Serial.print(reaction_time);
       Serial.println(" ms");
-      while(1){
-        
-      }
+
+      delay(2000); // Short pause before restarting test
+      restartTest();
     }
   }
 }
+
+void restartTest() {
+  Serial.println("Restarting...");
+  digitalWrite(RED_PIN, HIGH);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(BLUE_PIN, LOW);
+
+  set_time = millis() + random(6000, 11000);
+  testStarted = false;
+}
+
