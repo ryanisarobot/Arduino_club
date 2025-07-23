eca577bbbbed52d85a17fe6b77ad656212b0f7be
ryan <144874436+ryanisarobot@users.noreply.github.com>
2025-07-23 14:15:25 -0700

    Update Quick practice 1.ino

--- Quick practices/Quick practice 1.ino
+++ Quick practices/Quick practice 1.ino
@@ -1,17 +1,16 @@
-
 int LED_PINS[]{1, 2};
 
 void setup() {
   for(int x = 0; x < 2; x++){
     pinMode(LED_PINS[x], OUTPUT);
   } 
-  digitalWrite(1, HIGH);
+  digitalWrite(LED_PINS[0], HIGH);
 }
 
 void loop() {
   unsigned long timePassed = millis();
-  if(timePassed > 10000){
-    digitalWrite(2, HIGH);
-    digitalWrite(1, LOW);
+  if(timePassed > 5000){
+    digitalWrite(LED_PINS[1], HIGH);
+    digitalWrite(LED_PINS[0], LOW);
   }
 }
