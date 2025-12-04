f03183494a7352239429eb15970afecc5ae23cfc
ryan <144874436+ryanisarobot@users.noreply.github.com>
2025-12-03 22:55:47 -0700

    Refactor comments and improve code readability
    Corrected comments and formatting for clarity.

--- Project 7 - RC car/Project 7.ino
+++ Project 7 - RC car/Project 7.ino
@@ -1,15 +1,15 @@
-#include<IRremote.h> # Libary that gives us teh functions and 
-const int RemotePin = 8; # What pin we will set the inputs that the IR sensor recieves to go to
-IRrecv irrecv(RemotePin);
-decode_results results;
-int in1=3;
-int in2=5;
-int in3=6;
-int in4=9;
+#include<IRremote.h> // Libary that gives us teh functions and 
+const int RemotePin = 8; // What pin we will set the inputs that the IR sensor recieves to go to
+IRrecv irrecv(RemotePin); // This is teh instance of the class that we will define as "our specific IR sensor"
+decode_results results; // Holds all information decodes from IR sensor
+int in1 = 3;
+int in2 = 5;
+int in3 = 6;
+int in4 = 9;
 
 void setup() {
   Serial.begin(9600);
-  irrecv.enableIRIn();
+  irrecv.enableIRIn(); // Simply starts up the IR module
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
@@ -19,9 +19,9 @@ void setup() {
 
 void loop() {
   
-    if(irrecv.decode(&results))
+    if(irrecv.decode(&results)) // The function inside checks if there has been a signal and returns TRUE or FALSE
       {
-        if (results.value==0xFF629D)//Press UP Button
+        if (results.value==0xFF629D)//Press UP Button, this code is the hex code for the specific up button
         { 
           Forward();
         }
@@ -41,11 +41,12 @@ void loop() {
         { 
           Stop();
         }
-      irrecv.resume();
+      irrecv.resume(); // Returns that it is done processing and is ready for a new input
     }
     }
    
- 
+ // Movement functions
+
 void Backward()
   {
   digitalWrite(in1,HIGH);
