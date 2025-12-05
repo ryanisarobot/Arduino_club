930c542ef0ee46770f7e7e2d5db7da507735a570
ryan <144874436+ryanisarobot@users.noreply.github.com>
2025-12-04 22:09:24 -0700

    Update IR library usage and refactor code

--- Project 7 - RC car/Project 7.ino
+++ Project 7 - RC car/Project 7.ino
@@ -1,7 +1,5 @@
-#include<IRremote.h> // Libary that gives us teh functions and 
+#include<IRremote.hpp> // Libary that gives us teh functions and 
 const int RemotePin = 8; // What pin we will set the inputs that the IR sensor recieves to go to
-IRrecv irrecv(RemotePin); // This is teh instance of the class that we will define as "our specific IR sensor"
-decode_results results; // Holds all information decodes from IR sensor
 int in1 = 2;
 int in2 = 3;
 int in3 = 4;
@@ -11,7 +9,7 @@ const unsigned long delaytime = 200;
 
 void setup() {
   Serial.begin(9600);
-  irrecv.enableIRIn(); // Simply starts up the IR module
+  IrReceiver.begin(RemotePin, ENABLE_LED_FEEDBACK);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
@@ -20,30 +18,32 @@ void setup() {
 }
 
 void loop() {
-  if(irrecv.decode(&results)) // The function inside checks if there has been a signal and returns TRUE or FALSE
+  if(IrReceiver.decode()) // The function inside checks if there has been a signal and returns TRUE or FALSE
     {
-      if (results.value==0xB946FF00)//Press UP Button, this code is the hex code for the specific up button
+    unsigned long code = IrReceiver.decodedIRData.decodedRawData;
+
+      if (code==0xB946FF00)//Press UP Button, this code is the hex code for the specific up button
       { 
         Forward();
       }
-      else if (results.value==0xEA15FF00)//Press Down Button
+      else if (code==0xEA15FF00)//Press Down Button
       { 
         Backward();
       }
-      else if (results.value==0xBB44FF00)//Press Left Button
+      else if (code==0xBB44FF00)//Press Left Button
       { 
         Left();
       }
-      else if (results.value==0xBC43FF00)//Press Right Button
+      else if (code==0xBC43FF00)//Press Right Button
       { 
         Right();
       }
-      else if (results.value==0xBA45FF00)//Stop
+      else if (code==0xBA45FF00)//Stop
       { 
         Stop();
       }
     timesincelastinput = millis();
-    irrecv.resume(); // Returns that it is done processing and is ready for a new input
+    IrReceiver.resume(); // Returns that it is done processing and is ready for a new input
   }
   else if (millis() - timesincelastinput > delaytime)
   {
