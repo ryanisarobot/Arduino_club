585fe07826d7aeac40f0dba4e356f798c9adcf6c
ryan <144874436+ryanisarobot@users.noreply.github.com>
2025-12-04 22:03:10 -0700

    Refactor RC car control logic and add timeout

--- Project 7 - RC car/Project 7.ino
+++ Project 7 - RC car/Project 7.ino
@@ -6,6 +6,8 @@ int in1 = 2;
 int in2 = 3;
 int in3 = 4;
 int in4 = 5;
+unsigned long timesincelastinput = 0;
+const unsigned long delaytime = 200;
 
 void setup() {
   Serial.begin(9600);
@@ -14,36 +16,41 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-    
+  timesincelastinput = millis();  // initialize baseline
 }
 
 void loop() {
-  
-    if(irrecv.decode(&results)) // The function inside checks if there has been a signal and returns TRUE or FALSE
-      {
-        if (results.value==0xB946FF00)//Press UP Button, this code is the hex code for the specific up button
-        { 
-          Forward();
-        }
-        else if (results.value==0xFFA857)//Press Down Button
-        { 
-          Backward();
-        }
-         else if (results.value==0xFF22DD)//Press Left Button
-        { 
-          Left();
-        }
-        else if (results.value==0xFFC23D)//Press Right Button
-        { 
-          Right();
-        }
-        else if (results.value==0xFF02FD)//Stop
-        { 
-          Stop();
-        }
-      irrecv.resume(); // Returns that it is done processing and is ready for a new input
-    }
-    }
+  if(irrecv.decode(&results)) // The function inside checks if there has been a signal and returns TRUE or FALSE
+    {
+      if (results.value==0xB946FF00)//Press UP Button, this code is the hex code for the specific up button
+      { 
+        Forward();
+      }
+      else if (results.value==0xEA15FF00)//Press Down Button
+      { 
+        Backward();
+      }
+      else if (results.value==0xBB44FF00)//Press Left Button
+      { 
+        Left();
+      }
+      else if (results.value==0xBC43FF00)//Press Right Button
+      { 
+        Right();
+      }
+      else if (results.value==0xBA45FF00)//Stop
+      { 
+        Stop();
+      }
+    timesincelastinput = millis();
+    irrecv.resume(); // Returns that it is done processing and is ready for a new input
+  }
+  else if (millis() - timesincelastinput > delaytime)
+  {
+    Stop();
+    timesincelastinput = millis();
+  }
+}
    
  // Movement functions
 
@@ -68,17 +75,17 @@ void Backward()
   digitalWrite(in3,LOW);
   digitalWrite(in4,LOW);
   }
-  int Left()
-    {
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
-    }
-  int Right()
-    {
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,LOW);
-    }
+void Left()
+  {
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  }
+void Right()
+  {
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  }
